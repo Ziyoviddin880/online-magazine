0719e7eb092ab8f30f90ce84ed9405d706e5b2cd
Ziyoviddin880 <ziyoviddinabdurashidov7@gmail.com>
2023-11-09 18:33:49 +0500

    Add product with responsive

--- src/container/product/Product.js
+++ src/container/product/Product.js
@@ -3,10 +3,38 @@ import { useParams } from "react-router-dom";
 import { useFetch } from "../../useFetch/useFetch";
 
 function Product(props) {
-  const id = useParams();
-  const { data } = useFetch("https://fakestoreapi.com/products/" + id.id);
-  console.log(data);
-  return <h1>Product</h1>;
+  const { id } = useParams();
+  const { data, loading } = useFetch("https://fakestoreapi.com/products/" + id);
+  return (
+    <>
+      <div className="container">
+        <div className="product">
+          {console.log(loading)}
+          {loading && (
+            <div className="loading">
+              <img src="images/loading-gif.gif" alt="Loading" />
+            </div>
+          )}
+          {data && (
+            <div className="product-element">
+              <div className="product-img">
+                <img src={data.image} alt="Product" />
+              </div>
+              <div className="product-info">
+                <div className="title">{data.title.split(".")[0]}</div>
+                <div className="description">{data.description}</div>
+                <div className="price">
+                  <span>Price: </span>
+                  {data.price}$
+                </div>
+                <button className="product-btn">Button</button>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Product;
